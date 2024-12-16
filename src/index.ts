8eaa6d1fb69ce4a4dc13a0c3822302edf21d675a
longdog <linda@longdog.us>
2024-12-16 10:43:50 +0400

    add tarologist fabric

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
 import { gigachatTarologist } from "./gigachat";
+import { ITarologist } from "./model";
 import { makeTranslation } from "./prompt";
 import { makeSpreadGen } from "./spread";
 import { testTarologist } from "./testchat";
 import { tgService } from "./tgService";
 import { makeWeb } from "./web";
 
+
+const {AI_TYPE} = process.env;
+
+const tarologists = {
+  test: testTarologist,
+  gigachat: gigachatTarologist
+} as const
+
+const makeTarologist = (type?:string):Promise<ITarologist> => {
+  return tarologists[type as keyof typeof tarologists || "test"]()
+}
+
 export default makeWeb(
   makeSpreadGen(),
-  // await gigachatTarologist(),
-  await testTarologist(),
+  await makeTarologist(AI_TYPE),
   await makeTranslation(),
   tgService()
 );
